f946de584064df2f95aadf72751bbd5f5238b589
Cody Jones <codyjones@CodysDesktop.localdomain>
2023-04-20 22:53:50 -0500

    Added Register,Login,and Error Pages

--- src/components/GameGram.js
+++ src/components/GameGram.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Profile } from "./Pages/Profile";
+import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
+import { Error } from "./Pages/Error";
 
 export const GameGram = () => {
     return(
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/" element={<Home/>}/>
+            <Route path="/" element={<Home/>} errorElement={<Error/>}/>
+            <Route path="/profile" element={<Profile/>} errorElement={<Error/>}/>
+            <Route path="/login" element={<Login/>} errorElement={<Error/>}/>
+            <Route path="/register" element={<Register/>} errorElement={<Error/>}/>
         </Routes>
 
 
