9bffaddd31db38379cc7d49ae0308a0a0feb1067
Cody Jones <codyjones@CodysDesktop.localdomain>
2023-04-27 00:19:18 -0500

    CSS Modal

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { CreatePost } from "../Posts/CreatePost";
 export const Home = () => {
 
 
-    return(
+    return(<>
         <h1>Home</h1>
-    )
+        {<CreatePost/>}
+    </>)
 };
--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React from 'react';
+import Modal from 'react-modal';
+import "./CreatePost.css"
+export const CreatePost = () => {
 
-export const Home = () => {
-
-
-    return(
-        <h1>Home</h1>
-    )
-};
+    const modalStyle = {
+        content: {
+          width:"fit-content",
+          top: '50%',
+          left: '50%',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };    
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+  
+    function openModal() {
+      setIsOpen(true);
+    }
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
+  
+    return (
+      <div>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal}  style={modalStyle}>
+          <form className='Container'>
+          <button id='closeBtn' onClick={closeModal}>X</button>
+          <h1>Upload!</h1>
+          <h3 id='fileTxt'>Choose File!</h3>
+          <input id='fileUpload' type="file" name="filename"/>
+          <input id='submitBtn' type="submit"/>
+          <h3 id='descriptionTxt'>Title!</h3>
+          <input id='descriptionBox' type="text" name="fname"></input>
+          </form>
+        </Modal>
+      </div>
+    );
+  }
